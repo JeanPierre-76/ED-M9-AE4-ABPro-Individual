979876de16d7da8b1cf5369cff1cd09a17b84e75
JeanPierre-76 <jpgiusto@yahoo.com>
2022-11-09 23:58:25 -0300

    Prueba inicial

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+  has_many :posts, through: :boards
+  has_many :boards, dependent: :destroy
+  has_many :posts, dependent: :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+end
